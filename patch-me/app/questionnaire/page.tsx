9194f28fd5cdf190ec26d4cfa0a58d53bf3aa579
EmmaRinaldo <56730801+EmmaRinaldo@users.noreply.github.com>
2025-04-18 09:57:56 +0200

    g

--- patch-me/app/questionnaire/page.tsx
+++ patch-me/app/questionnaire/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { categories } from "@/utils/repairData"
+import icon from "../../public/cat1-l1.svg"
 
 const steps = ["Catégorie", "Type", "Problème", "Matériel", "Niveau"]
 
